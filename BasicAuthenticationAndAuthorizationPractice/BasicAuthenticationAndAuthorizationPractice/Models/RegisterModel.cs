3f144ecd2b0a6c13e6f1480a61a96976361f526d
sivakrishnareddy826 <sivakrishnareddy826@gmail.com>
2023-10-27 20:29:26 +0530

    Added Basic Authentication and authorization project with JWT Bearer token and Developed Role based authorization

---
+++ BasicAuthenticationAndAuthorizationPractice/BasicAuthenticationAndAuthorizationPractice/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicAuthenticationAndAuthorizationPractice.Models
+{
+    public class RegisterModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "User Name is required")]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
